acf835b8b7addb0f543b53625f5a924c90c63331
Prithiviraj R <prithiviraj7r@gmail.com>
2024-05-19 09:58:52 +0530

    Update utils.py

--- data_processing/utils.py
+++ data_processing/utils.py
@@ -4,6 +4,6 @@ import torch
 import shutil
 import zipfile
 from shutil import copyfile
-from multiprocessing import Pool
+from multiprocessing import Pool, Manager, Process
 from torch.utils.data import DataLoader
 from torchvision import transforms, models, datasets
